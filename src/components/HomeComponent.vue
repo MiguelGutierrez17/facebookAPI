2639f476bff18f624fc6a6b59687745c25e3c142
Miguel Bernal <mbernal@godoycordoba.com>
2023-05-01 21:09:56 -0500

    1v deploy working!

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>HOMO</h1>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log("HOME");
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: brown;
+  text-align: center;
+}
+</style>
